0da0cc5f5eff6f0089381c51f22595c4c9c270ae
rippanda12 <panda@bredos.org>
2024-08-15 00:51:56 +0000

    Revert changes in drivers/gpu/drm/rockchip/ made by commit 7b81ef361eda2e01447ee812a6baaf4303853019

--- drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
@@ -8334,14 +8334,7 @@ static int vop2_calc_dsc_clk(struct drm_crtc *crtc)
 	 * so when txp_clk is equal to v_pixclk, we set dsc_cds = crtc_clock / 4,
 	 * otherwise dsc_cds = crtc_clock / 8;
 	 */
-#ifdef CONFIG_DRM_PANEL_FYDETAB
-  if (output_if_is_mipi(vcstate->output_if))
-	  vcstate->dsc_cds_clk_rate = v_pixclk / (vcstate->dsc_txp_clk_rate == v_pixclk ? 4 : 8);
-  else
-    vcstate->dsc_cds_clk_rate = v_pixclk / 8;
-#else
-  vcstate->dsc_cds_clk_rate = v_pixclk / 8;
-#endif
+	vcstate->dsc_cds_clk_rate = v_pixclk / (vcstate->dsc_txp_clk_rate == v_pixclk ? 4 : 8);
 
 	return 0;
 }
@@ -8553,13 +8546,7 @@ static void vop2_crtc_enable_dsc(struct drm_crtc *crtc, struct drm_crtc_state *o
 	bool mipi_ds_mode = false;
 	uint32_t *reg_base = vop2->regs;
 	u32 offset = 0;
-#ifdef CONFIG_DRM_PANEL_FYDETAB
-  struct vop2_clk *dclk_core;
-  char clk_name[32];
 
-  snprintf(clk_name, sizeof(clk_name), "dclk_core%d", vp->id);
-  dclk_core = vop2_clk_get(vop2, clk_name);
-#endif
 	if (!vop2->data->nr_dscs) {
 		DRM_WARN("Unsupported DSC\n");
 
@@ -8669,14 +8656,8 @@ static void vop2_crtc_enable_dsc(struct drm_crtc *crtc, struct drm_crtc_state *o
 		 * dsc_htotal = htotal * (1 << dclk_core->div_val) /
 				((1 << dsc_txp_clk->div_val) * (1 << dsc_cds_clk->div_val))
 		*/
-#ifdef CONFIG_DRM_PANEL_FYDETAB
-    if (output_if_is_mipi(vcstate->output_if))
-      dsc_htotal = htotal * (1 << dsc_cds_clk->div_val) * (1 << dsc_txp_clk->div_val) / (1 << dclk_core->div_val);
-    else
-      dsc_htotal = htotal / (1 << dsc_cds_clk->div_val);
-#else
-    dsc_htotal = htotal / (1 << dsc_cds_clk->div_val);
-#endif
+		dsc_htotal = htotal * (1 << dclk_core->div_val) /
+				((1 << dsc_txp_clk->div_val) * (1 << dsc_cds_clk->div_val));
 		val = dsc_htotal << 16 | dsc_hsync;
 		VOP_MODULE_SET(vop2, dsc, dsc_htotal_pw, val);
 
